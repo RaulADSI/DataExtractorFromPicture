b04778463ee665231e1cfe7fada53208bf39ab9d
RaulADSI <rauramirez25@gmail.com>
2025-03-12 12:52:44 -0500

    Agregamos la funcionalidad de extraer texto desde una imagen

--- src/main/java/com/rulodev/dataextractorfrompicture/ExtractorMain.java
+++ src/main/java/com/rulodev/dataextractorfrompicture/ExtractorMain.java
@@ -7,17 +7,22 @@ package com.rulodev.dataextractorfrompicture;
 public class ExtractorMain {
 
     public static void main(String[] args) {
+         System.out.println("Extracción de texto de imágenes...");
 
-        System.out.println("Extracción de texto de imágenes...");
+        // Instancia de la clase que realizará la extracción
         TextExtractor textExtractor = new TextExtractor();
-        String imageDirectory = "C:\\Users\\strategic\\OneDrive\\Documentos\\output\\"; // Carpeta con las imágenes
-        String csvPath = "C:\\Users\\strategic\\OneDrive\\Documentos\\textoExtraido.csv"; // Ruta del archivo CSV
+
+        // Ruta a la carpeta que contiene las imágenes y al archivo CSV de salida
+        String imageDirectory = "C:\\Users\\strategic\\OneDrive\\Documentos\\Pdf_Picture";
+        String csvPath = "C:\\Users\\strategic\\OneDrive\\Documentos\\textoExtraido.csv";
 
         try {
             textExtractor.extractTextToCsv(imageDirectory, csvPath);
+            System.out.println("Extracción completada. Revisa el archivo CSV en: " + csvPath);
         } catch (Exception e) {
             System.err.println("Ocurrió un error durante la extracción: " + e.getMessage());
         }
-    }
+        
+ }
 }
 
